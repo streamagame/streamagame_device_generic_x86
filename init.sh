3c926bcad81f77e12ecf2b109b92416d4eebb8c1
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-11-02 14:55:23 +0800

    alsa: support restore alsa state from saved configs

--- init.sh
+++ init.sh
@@ -17,6 +17,14 @@ function init_misc()
 
 	# in case no cpu governor driver autoloads
 	[ -d /sys/devices/system/cpu/cpu0/cpufreq ] || modprobe acpi-cpufreq
+
+	case "$PRODUCT" in
+		T10*TA)
+			echo on > /sys/bus/platform/drivers/sdhci-acpi/INT33BB:00/power/control
+			;;
+		*)
+			;;
+	esac
 }
 
 function init_hal_audio()
@@ -28,21 +36,6 @@ function init_hal_audio()
 		*)
 			;;
 	esac
-	[ -d /proc/asound/card0 ] || modprobe snd-dummy
-
-	for c in $(grep '\[.*\]' /proc/asound/cards | awk '{print $1}'); do
-		alsa_ctl init $c
-		alsa_amixer -c $c set Master on
-		alsa_amixer -c $c set Master 100
-		alsa_amixer -c $c set Headphone on
-		alsa_amixer -c $c set Headphone 100
-		alsa_amixer -c $c set Speaker 100
-		alsa_amixer -c $c set Capture 100
-		alsa_amixer -c $c set Capture cap
-		alsa_amixer -c $c set PCM 100 unmute
-		alsa_amixer -c $c set 'Mic Boost' 3
-		alsa_amixer -c $c set 'Internal Mic Boost' 3
-	done
 }
 
 function init_hal_bluetooth()
@@ -343,6 +336,25 @@ function do_bootcomplete()
 		*)
 			;;
 	esac
+
+	for c in $(grep '\[.*\]' /proc/asound/cards | awk '{print $1}'); do
+		f=/system/etc/alsa/$(cat /proc/asound/card$c/id).state
+		if [ -e $f ]; then
+			alsa_ctl -f $f restore $c
+		else
+			alsa_ctl init $c
+			alsa_amixer -c $c set Master on
+			alsa_amixer -c $c set Master 100%
+			alsa_amixer -c $c set Headphone on
+			alsa_amixer -c $c set Headphone 100%
+			alsa_amixer -c $c set Speaker 100%
+			alsa_amixer -c $c set Capture 100%
+			alsa_amixer -c $c set Capture cap
+			alsa_amixer -c $c set PCM 100 unmute
+			alsa_amixer -c $c set 'Mic Boost' 3
+			alsa_amixer -c $c set 'Internal Mic Boost' 3
+		fi
+	done
 }
 
 PATH=/system/bin:/system/xbin
